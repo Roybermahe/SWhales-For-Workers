3b3ba0ef893b0a8b90eaacae5fc7e0e45bd3923d
Royber <roybermanjarrez@gmail.com>
2020-01-12 09:40:20 -0500

    Se actualiza app-routing
    versión estable sin estilos agregagos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
+import { inicio } from "./InicioDeSesion/inicio.component";
 
 
 const routes: Routes = [
+    {   path: "", redirectTo:"/InicioDeSesion", pathMatch:"full"   },
+    {   path: "InicioDeSesion", component: inicio   }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { inicio } from "./InicioDeSesion/inicio.component";
 
 @NgModule({
     bootstrap: [
@@ -13,7 +14,8 @@ import { AppComponent } from "./app.component";
         AppRoutingModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        inicio
     ],
     providers: [],
     schemas: [
